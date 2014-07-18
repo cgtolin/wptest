f29b0008f3545b55034e9a1a9592426facec7cbf
cgtolin <cgtolin@gmail.com>
2014-07-17 23:32:43 -0600

    3nd commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,17 +8,17 @@ require './config/slack'
 # Setup WordPress
 ############################################
 
-set :wp_user, "yourname" # The admin username
-set :wp_email, "yourname@example.com" # The admin email address
+set :wp_user, "cgtolin" # The admin username
+set :wp_email, "cgtolin@gmail.com" # The admin email address
 set :wp_sitename, "WP Deploy" # The site title
-set :wp_localurl, "http://wpdeploy" # Your local environment URL
+set :wp_localurl, "http://localhost" # Your local environment URL
 
 ############################################
 # Setup project
 ############################################
 
-set :application, "wp-deploy"
-set :repo_url, "git@github.com:Mixd/wp-deploy.git"
+set :application, "wp-test"
+set :repo_url, "git@github.com:cgtolin/wptest.git"
 set :scm, :git
 
 set :git_strategy, SubmoduleStrategy
