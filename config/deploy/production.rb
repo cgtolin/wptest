f9f9eb665afaa93f0a28c9cca6666f2d334d7474
cgtolin <cgtolin@gmail.com>
2014-07-18 00:01:32 -0600

    4th

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@
 
 set :stage, :production
 set :stage_url, "http://192.168.0.10/wpprod"
-server "192.168.0.10", user: "deploy", roles: %w{web app db}
+server "192.168.0.10", user: "vagrant", roles: %w{web app db}
 set :deploy_to, "/var/www/html/wpprod"
 
 ############################################
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,7 +4,7 @@
 
 set :stage, :staging
 set :stage_url, "http://192.168.0.10/wpstage"
-server "192.168.0.10", user: "deploy", roles: %w{web app db}
+server "192.168.0.10", user: "vagrant", roles: %w{web app db}
 set :deploy_to, "/var/www/html/wpstage"
 
 ############################################
